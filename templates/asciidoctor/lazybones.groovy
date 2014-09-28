8a0336daf30f4a2265b8577c5f88bab87bef64ec
Frank Becker <frank@calmdevelopment.de>
2014-09-28 16:34:19 +0200

    enable/disable asciidoctor-diagram

--- templates/asciidoctor/lazybones.groovy
+++ templates/asciidoctor/lazybones.groovy
@@ -5,6 +5,7 @@ props.projectName = ask("Set Project Name [$projectName]: ", projectName, "name"
 props.version = ask("Set Version [0.1]: ", "0.1", "version")
 props.author = ask("Give me a name of an Author: ", "", "author")
 props.email = ask("Give me an email Address: ", "", "email")
+props.diagram = ask("Do you want to enable asciidoctor-diagram plugin? (default: no) ","","diagram")
 
 def files = [ "src/asciidoc/index.adoc", "gradle/asciidoctor.gradle", "gradle.properties" ]
 

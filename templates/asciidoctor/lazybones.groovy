9c42da10a641ea8516eab21e4070e55c03717012
Frank Becker <frank@calmdevelopment.de>
2016-06-25 12:16:02 +0200

    update asciidoctor template to use different tasks for pdf and html5 backend

--- templates/asciidoctor/lazybones.groovy
+++ templates/asciidoctor/lazybones.groovy
@@ -1,18 +1,12 @@
 def props = [:]
 def projectName = projectDir.getName()
 
-def defaultGemPath = '/var/lib/gems/1.9.1'
-
 props.projectName = ask("Set Project Name [$projectName]: ", projectName, "name")
 props.version = ask("Set Version [0.1]: ", "0.1", "version")
 props.author = ask("Give me a name of an Author: ", "", "author")
 props.email = ask("Give me an email Address: ", "", "email")
+props.asciidoctorVersion = ask("Set asciidoctor version [1.5.4]: ", "1.5.4", "asciidoctorVersion")
 props.diagram = ask("Do you want to enable asciidoctor-diagram plugin? (default: no) ","","diagram")
-props.gemPath = defaultGemPath
-
-if ( props.diagram ) {
-    props.gemPath = ask("Where is the location of your Ruby gems-Path? [$defaultGemPath] ", defaultGemPath, "gemPath")
-}
 
 def files = [ "src/docs/asciidoc/index.adoc", "gradle/asciidoctor.gradle", "gradle.properties" ]
 
